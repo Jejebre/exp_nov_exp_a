52a3913765f3d2e6f538c56de734964411139d4b
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-06-21 12:51:58 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter.py
+++ Exercise 3 - Transporteur_A/Transporter.py
@@ -10,12 +10,12 @@ def prox():
         
     nf_leds_top(32,16,0) # Orange
 
-    if prox_horizontal[2] > 2500:
+    if prox_horizontal[2] > 3000:
         nf_leds_top(0,32,0) # Green
         motor_left_target = -251 
         motor_right_target = -251
         
-    elif prox_horizontal[6] > 2500: 
+    elif prox_horizontal[6] > 3000: 
         nf_leds_top(32,0,0) # Red
         motor_left_target = 251
         motor_right_target = speed
