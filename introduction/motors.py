83092c1267bf24705cfbcbe258c6d6307137261f
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-06-21 12:33:22 +0200

    autosave

--- introduction/motors.py
+++ introduction/motors.py
@@ -1,8 +1,5 @@
 # In this window is the program you want to send.
 
 # For example, we want Thymio's wheel to turn at 150 speed forward
-motor_right_target = 150
+motor_right_target = 0
 motor_left_target = 150
-if button_forward == True:
-    motor_right_target = 0
-    motor_left_target = 0
