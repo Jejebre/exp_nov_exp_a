2fe30afe8d7d413538e2ba3c2011f18b0f54fb87
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-06-24 11:40:34 +0200

    autosave

--- Exercise 1 - Autour du Jardin_A/Garden_nov.py
+++ Exercise 1 - Autour du Jardin_A/Garden_nov.py
@@ -17,7 +17,7 @@ def timer0():
         timer_period[0] = turn_time
         nf_leds_top(32,16,0) # Orange
         motor_right_target = 250
-        motor_left_target = -150
+        motor_left_target = -250
         
 
 
