cf5e71dc7d35acf54dcff1c1f1bb288afe0ef5d8
jerome <jerome@stidhcp-4-059.epfl.ch>
2024-06-25 16:02:06 +0200

    autosave

--- introduction/prox.py
+++ introduction/prox.py
@@ -4,5 +4,5 @@ def prox(): # The block of block below will be executed each time Thymio senses 
     
     # console displays 0 meaning there's no wall in front
     # console displays 2500 meaning there's a wall at the back
-    print(prox_horizontal[2]) # it is for the prox 2 !!  
+    print(prox_horizontal[6]) # it is for the prox 2 !!  
 
