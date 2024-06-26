6d3d69c2588265611cb658fb72e5dd35b2d7311a
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-06-26 18:30:33 +0200

    autosave

--- Exercise 3 - Transporteur_A/Key.py
+++ Exercise 3 - Transporteur_A/Key.py
@@ -12,7 +12,7 @@ def prox():
         nf_leds_top(0,32,0) # Green
         motor_left_target = -251 
         motor_right_target = 251
-        
+        print("la")
     else: 
         nf_leds_top(32,0,0) # Red
         motor_left_target = 251
