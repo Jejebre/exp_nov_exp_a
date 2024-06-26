e38e3125cdf05252209f672343b7b91302d54c4c
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-06-26 18:15:55 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -22,15 +22,15 @@ def prox():
     
     elif mode == WALLS:
         nf_leds_top(32, 0, 0)  # Red
-        
+        if prox_horizontal[2] > 3500:
+            motor_left_target = -251
+            motor_left_target = -251 
         elif prox_ground_delta[0] < 800 or prox_ground_delta[1] < 800:
             mode = LINE
             timer_period[0] = 1950 #turn during 1950ms            
             motor_left_target = -251
             motor_right_target = 251
-        if:
-            motor_left_target = -251
-            motor_left_target = -251
+
 
 @onevent
 def timer0():
