599fc106d8fb4ee5c092cc9483c91b2dfbbfeadd
jerome <jerome@stidhcp-4-059.epfl.ch>
2024-07-02 11:56:20 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -26,8 +26,9 @@ def prox():
         if prox_ground_delta[0] < 800 or prox_ground_delta[1] < 800:
             mode = LINE
             timer_period[0] = 1950 #turn during 1950ms
+            print("backward")
             motor_left_target = -251
-            motor_right_target = 251
+            motor_right_target = -251
 
 @onevent
 def timer0():
