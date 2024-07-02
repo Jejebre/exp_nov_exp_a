4d9b38ce17f251ba53fbf8730edc52ef3a2cbd5d
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-07-02 14:03:01 +0200

    autosave

--- Exercise 2 - Suis mes ordres_A/Myorders_exp.py
+++ Exercise 2 - Suis mes ordres_A/Myorders_exp.py
@@ -4,7 +4,7 @@ def buttons():
     
     if button_forward:
         nf_leds_top(0,32,0)
-        motor_left_target == 255
+        motor_left_target = 255
         motor_right_target = 255
         
     if button_left:
@@ -17,7 +17,7 @@ def buttons():
         motor_left_target = 100
         motor_right_target = -100
         
-    if button_forward:
+    if button_backward:
         nf_leds_top(32,0,0)
         motor_left_target = -255
         motor_right_target = -255
