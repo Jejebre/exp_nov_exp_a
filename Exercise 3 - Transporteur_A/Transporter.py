c2fc47ec989c54728c69594e5480528b683b4369
jerome <jerome@stidhcp-4-033.epfl.ch>
2024-07-03 12:25:56 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter.py
+++ Exercise 3 - Transporteur_A/Transporter.py
@@ -15,10 +15,10 @@ def prox():
         motor_left_target = 0
         motor_right_target = 0
         
-    elif prox_horizontal[7] > 2500: 
+    elif prox_horizontal[5] > 2500: 
         nf_leds_top(32,0,0) # Red
         motor_left_target = 0
-        motor_right_target = 251
+        motor_right_target = 0
         
 
 
