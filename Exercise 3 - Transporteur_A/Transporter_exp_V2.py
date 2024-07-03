3680297910b7c58bc23a7fe29d25ff42e0023432
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-07-03 18:27:20 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -14,7 +14,10 @@ def prox():
     if mode == NORMAL:
         nf_leds_top(0, 32, 0)  # Green
         
-        if prox_ho
+        if prox_horizontal[2] > 3000:
+            mode = WALLS
+            motor_left_target = -251
+            motor_right_tarfet = -251
 #         if prox_horizontal[2] < 2500 or prox_horizontal[1] > 2500:
 #             mode = WALLS
 #             motor_left_target = -251 
