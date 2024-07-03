9c7834440f9bba2d55e3a6d7c4d1e307204945e8
jerome <jerome@stidhcp-4-033.epfl.ch>
2024-07-03 12:14:28 +0200

    autosave

--- Exercise 1 - Autour du Jardin_A/Garden_nov.py
+++ Exercise 1 - Autour du Jardin_A/Garden_nov.py
@@ -1,4 +1,4 @@
-forward_time = 2500
+forward_time = 2700
 turn_time = 1150
 timer_period[0] = 100
 going_forward = False
@@ -16,8 +16,8 @@ def timer0():
     else:
         timer_period[0] = turn_time
         nf_leds_top(32,16,0) # Orange
-        motor_right_target = 250
-        motor_left_target = -250
+        motor_right_target = 220
+        motor_left_target = -220
         
 
 
