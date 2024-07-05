d6b10fe27925328f64f50c40ee1b102a4f020482
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-07-05 13:02:34 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -28,9 +28,10 @@ def prox():
         
         if prox_ground_delta[0] < 800 or prox_ground_delta[1] < 800:
             mode = LINE
-            timer_period[0] = 1800 #turn during 1950ms            
             motor_left_target = -251
             motor_right_target = 251
+            timer_period[0] = 1800 #turn during 1950ms            
+
 
 @onevent
 def timer0():
