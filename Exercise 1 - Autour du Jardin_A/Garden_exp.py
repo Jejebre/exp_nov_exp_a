dea81bc42efdfb8645efcdb551f683bb48372144
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-07-05 12:37:35 +0200

    autosave

--- Exercise 1 - Autour du Jardin_A/Garden_exp.py
+++ Exercise 1 - Autour du Jardin_A/Garden_exp.py
@@ -1,4 +1,4 @@
-time_A = 2500
+time_A = 3000
 time_B = 1100
 timer_period[0] = 100
 going_forward = False
@@ -16,8 +16,8 @@ def timer0():
     else:
         timer_period[0] = time_B
         nf_leds_top(32,16,0) # Orange
-        motor_right_target = -200
-        motor_left_target = 200
+        motor_right_target = 200
+        motor_left_target = -200
         
 
 # ------------------------------------------------------------- #
