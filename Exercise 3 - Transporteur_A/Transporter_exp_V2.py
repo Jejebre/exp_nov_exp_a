980c8e32036ae0fcf6bc4f6aa823968e0148a396
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-11-15 16:48:33 +0100

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -40,7 +40,7 @@ def timer0():
         motor_right_target = 251
         nf_leds_top(32, 32, 0) # Orange
         mode = NORMAL
-        timer_period[0] = 1950
+        timer_period[0] = 10
         
 @onevent
 def buttons():
