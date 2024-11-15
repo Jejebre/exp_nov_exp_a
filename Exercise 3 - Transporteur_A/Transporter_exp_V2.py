30789bf0c847963846b188db9499ca75e8d39fe0
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-11-15 16:38:09 +0100

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -16,7 +16,7 @@ def prox():
         motor_left_target = 251
         motor_right_target = 251# Green
         
-        if prox_horizontal[2] < 2500:
+        if prox_horizontal[2] > 4000:
             mode = WALLS
             motor_left_target = -251 
             motor_right_target = -251
