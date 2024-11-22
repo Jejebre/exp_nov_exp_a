503d010415f54a1e6a9088c3aaaeee2b8b815f78
jerome <jerome@tsf-516-wpa-7-102.epfl.ch>
2024-11-22 17:38:23 +0100

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -1,7 +1,7 @@
 # Constants
 NORMAL = 0
 WALLS = 1
-LINE = 2
+TURN = 2
 STOP = 3
 
 # Start
@@ -27,7 +27,7 @@ def prox():
         nf_leds_top(32, 0, 0)  # Red
         
         if prox_ground_delta[0] < 800 or prox_ground_delta[1] < 800:
-            mode = LINE
+            mode = TURN
             timer_period[0] = 2380 #turn during 1950ms
             
             motor_left_target = -200
@@ -37,7 +37,7 @@ def prox():
 def timer0():
     global mode, motor_left_target, motor_right_target
     # Check if the current mode is LINE to switch back to NORMAL
-    if mode == LINE:
+    if mode == TURN:
         motor_left_target = 100
         motor_right_target = 100
         nf_leds_top(32, 32, 0) # Orange
