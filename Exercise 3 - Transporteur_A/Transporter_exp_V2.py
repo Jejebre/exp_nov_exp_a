d4b538b25e2d2d5ecfd94f329d730323ead05df7
jerome <jerome@tsf-516-wpa-7-102.epfl.ch>
2024-11-22 17:31:09 +0100

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -6,6 +6,8 @@ STOP = 3
 
 # Start
 mode = NORMAL
+motor_left_target = 100 
+motor_right_target = 100
 
 @onevent
 def prox():
@@ -16,8 +18,8 @@ def prox():
         
         if prox_horizontal[2] > 2500 or prox_horizontal[1] > 2500 or prox_horizontal[3] > 2500:
             mode = WALLS
-            motor_left_target = -251 
-            motor_right_target = -251
+            motor_left_target = -100
+            motor_right_target = -100
             
     
     elif mode == WALLS:
@@ -26,16 +28,16 @@ def prox():
         if prox_ground_delta[0] < 800 or prox_ground_delta[1] < 800:
             mode = LINE
             timer_period[0] = 1950 #turn during 1950ms            
-            motor_left_target = -251
-            motor_right_target = 251
+            motor_left_target = -100
+            motor_right_target = 100
 
 @onevent
 def timer0():
     global mode, motor_left_target, motor_right_target
     # Check if the current mode is LINE to switch back to NORMAL
     if mode == LINE:
-        motor_left_target = 251 
-        motor_right_target = 251
+        motor_left_target = 100
+        motor_right_target = 100
         nf_leds_top(32, 32, 0) # Orange
         mode = NORMAL
         
