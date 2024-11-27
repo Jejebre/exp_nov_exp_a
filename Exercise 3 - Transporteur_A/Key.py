5016a9b7c26a70efc69f99cd3ebee2fc4e72382a
jerome <jerome@Mobotss-MacBook-Pro.local>
2024-11-27 10:49:04 +0100

    autosave

--- Exercise 3 - Transporteur_A/Key.py
+++ Exercise 3 - Transporteur_A/Key.py
@@ -7,8 +7,8 @@ back = 6
 @onevent
 def prox():
     global prox_horizontal, motor_left_target, motor_right_target
-        
-    if prox_horizontal[2] > 4000:
+    
+    if prox_horizontal[2] > 2000:
         nf_leds_top(0,32,0) # Green
         motor_left_target = -251 
         motor_right_target = 251
