463a6fdb8278356ecb9acca8e69105bc1c55f20b
jerome <jerome@stidhcp-4-103.epfl.ch>
2024-11-27 15:59:50 +0100

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -13,8 +13,10 @@ def prox():
 
     if mode == NORMAL:
         nf_leds_top(0, 32, 0)  # Green
-        motor
-        if prox_horizontal[2] > 2500 or prox_horizontal[1] > 2500 or prox_horizontal[3] > 2500:
+        motor_left_target = 250
+        motor_right_target = 250
+        
+        if prox_horizontal[2] > 4000 or prox_horizontal[1] > 4000 or prox_horizontal[3] > 2500:
             mode = WALLS
             motor_left_target = -251 
             motor_right_target = -251
