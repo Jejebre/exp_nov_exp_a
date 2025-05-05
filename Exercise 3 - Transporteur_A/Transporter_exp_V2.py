bb7a8a5d1e291da65c5ba886b73cd489d582b9fa
jerome <jerome@tsf-516-wpa-1-211.epfl.ch>
2025-05-05 16:54:57 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -13,7 +13,7 @@ def prox():
 
     if mode == NORMAL:
         nf_leds_top(0, 32, 0)
-        motor_left_target = -251
+        motor_left_target = 251
         motor_right_target = 251# Green
         
         if prox_horizontal[2] > 2500 or prox_horizontal[1] > 2500:
@@ -25,7 +25,7 @@ def prox():
     elif mode == WALLS:
         nf_leds_top(32, 0, 0)  # Red
         
-        if prox_ground_delta[0] < 800 or prox_ground_delta[1] < 800:
+        if prox_ground_delta[0] < 500 or prox_ground_delta[1] < 500:
             mode = LINE
             timer_period[0] = 1950 #turn during 1950ms            
             motor_left_target = -251
