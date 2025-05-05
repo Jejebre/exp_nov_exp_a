e3112bf8c64410646b623123e79c35fb8d20cbec
jerome <jerome@Mobotss-MacBook-Pro.local>
2025-05-05 15:46:56 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -21,13 +21,13 @@ def buttons():
 @onevent
 def timer0():
     global leds_top, going_forward, motor_left_target, motor_right_target
-#     going_forward = not going_forward
+    going_forward = not going_forward
     way_modif = (mode==FORWARD * 2 + 1)
     if going_forward:
         timer_period[0] = forward_time
         nf_leds_top(0,32,0) # Green
         motor_right_target = 250*way_modif
-        motor_left_target = 250*way_modif
+        motor_left_target = -250*way_modif
     else:
         timer_period[0] = turn_time
         nf_leds_top(32,16,0) # Orange
