73d0b0abbbdce2720271945a2deda387ee76c965
jerome <jerome@tsf-516-wpa-4-104.epfl.ch>
2025-05-09 14:44:28 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -1,5 +1,38 @@
+FORWARD = 0
+BACKWARD = 1
+forward_time = 1800
+turn_time = 1100
 
-        
+going_forward = False
+timer_period[0] = 100
+
+
+mode = FORWARD
+
+@onevent
+def buttons():
+    global mode, motor_right_target, motor_left_target, going_forward
+    if button_forward:
+        mode=FORWARD
+    if button_backward:
+        mode=BACKWARD
+    going_forward = not going_forward
+
+@onevent
+def timer0():
+    global leds_top, going_forward, motor_left_target, motor_right_target
+    going_forward = not going_forward
+    way_modif = (mode==FORWARD * 2 + 1)
+    if going_forward:
+        timer_period[0] = forward_time
+        nf_leds_top(0,32,0) # Green
+        motor_right_target = 250*way_modif
+        motor_left_target = 250*way_modif
+    else:
+        timer_period[0] = turn_time
+        nf_leds_top(32,16,0) # Orange
+        motor_right_target = -200*way_modif
+        motor_left_target = 200*way_modif       
         
         
         
