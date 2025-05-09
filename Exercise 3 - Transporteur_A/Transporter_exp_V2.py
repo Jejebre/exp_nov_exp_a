dc1cd17ec583b5a6c03c64f49f9f34a6a48fca04
jerome <jerome@tsf-516-wpa-4-104.epfl.ch>
2025-05-09 11:38:40 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -26,13 +26,13 @@ def timer0():
     if going_forward:
         timer_period[0] = forward_time
         nf_leds_top(0,32,0) # Green
-        motor_right_target = 250*way_modif
-        motor_left_target = 250*way_modif
+        motor_right_target = 250*way_modif*mode
+        motor_left_target = 250*way_modif*mode
     else:
         timer_period[0] = turn_time
         nf_leds_top(32,16,0) # Orange
-        motor_right_target = -200*way_modif
-        motor_left_target = 200*way_modif
+        motor_right_target = -200*way_modif*mode
+        motor_left_target = 200*way_modif*mode
         
 # ------------------------------------------------------------- #
 # ------------------------------------------------------------- #
