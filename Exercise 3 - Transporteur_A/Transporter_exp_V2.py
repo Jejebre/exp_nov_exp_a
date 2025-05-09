f6605ede459fde59517549078db847888a6a8c00
jerome <jerome@tsf-516-wpa-4-104.epfl.ch>
2025-05-09 10:19:13 +0200

    autosave

--- Exercise 3 - Transporteur_A/Transporter_exp_V2.py
+++ Exercise 3 - Transporteur_A/Transporter_exp_V2.py
@@ -16,7 +16,7 @@ def buttons():
         mode=FORWARD
     if button_backward:
         mode=BACKWARD
-    going_forward = not going_forward
+        going_forward = not going_forward
     
 @onevent
 def timer0():
